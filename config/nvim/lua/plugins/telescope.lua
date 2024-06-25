0199d4377850860b56fc3efcc269c47ea17ca905
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2024-06-25 15:23:47 +0800

    fix: fix telescope configuration

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -2,11 +2,10 @@ return {
   "nvim-telescope/telescope.nvim",
   opts = {
     defaults = {
-      path_display = { "smart " },
+      path_display = { "smart" },
       prompt_prefix = "🐿 ",
       selection_caret = "🐹 ",
       dynamic_preview_title = true, -- always show the title on the preview pane
-      layout_strategy = "vertical",
     }
   }
 }
