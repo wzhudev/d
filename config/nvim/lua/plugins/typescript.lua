81bd086a2b906be779ee13fa1b1572dcbe324b73
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2024-06-28 09:41:35 +0800

    feat: update typescript tools

--- config/nvim/lua/plugins/typescript.lua
+++ config/nvim/lua/plugins/typescript.lua
@@ -1,3 +1,15 @@
 return {
-  -- disable inline type hints
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+      ensure_installed = {
+        "eslint_d"
+      },
+    },
+  }
 }
