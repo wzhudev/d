1760c12cfde34b735626e68e1efe9cf6524aefc4
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2024-07-04 13:32:24 +0800

    feat: add rulers

---
+++ config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "lukas-reineke/virt-column.nvim",
+    opts = {},
+    lazy = false,
+    config = function()
+      require("virt-column").setup({
+        char = "╎",
+        virtcolumn = "+1,120,+1,80"
+      })
+    end
+  },
+}
