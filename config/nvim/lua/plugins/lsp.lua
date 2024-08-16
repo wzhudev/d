551b55a17641a43565fa479260ede50632ee3f37
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2024-08-16 18:52:48 +0800

    feat: remove usage

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -110,13 +110,13 @@ return {
       })
     end
   },
-  {
-    'Wansmer/symbol-usage.nvim',
-    event = 'BufReadPre', -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
-    config = function()
-      require('symbol-usage').setup({
-        text_format = text_format,
-      })
-    end
-  }
+  -- {
+  --   'Wansmer/symbol-usage.nvim',
+  --   event = 'BufReadPre', -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
+  --   config = function()
+  --     require('symbol-usage').setup({
+  --       text_format = text_format,
+  --     })
+  --   end
+  -- }
 }
