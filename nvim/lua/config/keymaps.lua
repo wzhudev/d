a76ef3bdfbfe3e3c42aa24ff07b19a3108584bec
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2024-12-21 12:11:02 +0800

    fix: fix tmux navigation

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,12 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
+
+local nvim_tmux_nav = require("nvim-tmux-navigation")
+
+vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
